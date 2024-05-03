8ac52ffd8c3125327a468f40b9a8b98e2eb47d21
Sameer Sharma <sameersharma0140@gmail.com>
2024-05-03 22:37:48 +0530

    Decided the layout and functions

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    // <div className="text-3xl font-bold text-3xl">
+    //   Namaste Everyone
+    // </div>
+    <h1 className="text-3xl font-bold underline">
+    Hello world! Namaste Netflix gpt
+  </h1>
+  );
+}
+
+export default App;
